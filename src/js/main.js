7de1f97cee914baef56b7d0c65034192d7b8b714
Dmytro Kornienko <caxapz1q@gmail.com>
2025-10-14 22:12:18 +0300

    feat: add main layout, hero section and base components

---
+++ src/js/main.js
@@ -0,0 +1,43 @@
+import Swiper from 'swiper';
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+import { Navigation, Pagination } from 'swiper/modules';
+
+const swiper = new Swiper(".mySwiper", {
+    modules: [Navigation, Pagination],
+    slidesPerView: 4,
+    loop: true,
+    spaceBetween: 40,
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+        clickable: true,
+
+        pagination: {
+            el: ".swiper-pagination",
+            clickable: true,
+        },
+    }
+});
+
+try {
+    const tabs = document.querySelectorAll(".schedule__btn");
+    const contents = document.querySelectorAll(".schedule__item");
+
+    tabs.forEach((tab, index) => {
+        tab.addEventListener("click", () => {
+            // Удаляем активный класс у всех табов и контента
+            tabs.forEach((t) => t.classList.remove("schedule__btn-active"));
+            contents.forEach((c) => (c.style.display = "none"));
+
+            // Добавляем активный класс к нажатому табу и показываем соответствующий контент
+            tab.classList.add("schedule__btn-active");
+            contents[index].style.display = "flex";
+        });
+    });
+
+    // Показываем первый контент при загрузке
+    contents.forEach((c, i) => (c.style.display = i === 0 ? "flex" : "none"));
+} catch (e) { }
+
